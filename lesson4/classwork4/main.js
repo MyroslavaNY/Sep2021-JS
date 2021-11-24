332817d6a93a0da1425a34ba01f77b2196f3785e
MyroslavaNY <MyroslavaNY@gmail.com>
2021-11-24 08:47:12 +0200

    lesson 4

--- lesson4/classwork4/main.js
+++ lesson4/classwork4/main.js
@@ -30,8 +30,9 @@
 // aboutMax(3,2,5)
 // - створити функцію яка повертає найбільше число з масиву
 
-// let maxNum = 0;
+
 // function aboutNumMax() {
+//     let maxNum = [0];
 //     for (let argument of arguments) {
 //         if (argument>=maxNum){
 //             maxNum=argument;
@@ -42,13 +43,12 @@
 // }
 // aboutNumMax(2,5,7,9,22,3,112,46)
 // - створити функцію яка повертає найменьше число з масиву
-// let minNum = 0;
 // function aboutNumMin() {
+//     let minNum = [0];
 //     for (let argument of arguments) {
 //         if (argument<=minNum){
 //             minNum=argument;
 //         }
-//
 //     }
 //     console.log(minNum);
 //         return minNum;
@@ -58,7 +58,7 @@
 
 
 // - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13
-// function summNum() {
+// function summNum(arguments) {
 //     let summ = 0;
 //     for (let argument of arguments) {
 //         summ=summ+argument;
@@ -71,27 +71,65 @@
 
 
 // - створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
-// function aboutMathMean() {
-//     let mathMean = 0;
-//     for (let argument of arguments) {
-//         mathMean=(mathMean+argument)/mathMean[argument];
 //
+// function aboutMathMean(arr) {
+//     let sum=0;
+//     let mathMean=sum/arr.length;
+//     for (const item of arr) {
+//         sum =sum+item;
 //     }
 //     return mathMean;
 // }
-// let mathMean1= aboutMathMean(2,3,5,6);
-// console.log(mathMean1)
+// aboutMathMean(6,5,2,5,4)
 // - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено);
-
+// function numMinMax() {
+//     let min = [0];
+//     let max = [0];
+//     for (let element of arguments) {
+//         if (element>max){
+//             max=element;
+//             console.log(max);
+//         }
+//         if (element<min){
+//             min=element;
+//         }
+//     }
+//     return min;
+//
+// }
+// numMinMax(6,3,5,88,12,45);
 
 
 // - створити функцію яка заповнює масив рандомними числами
 // (цей код генерує рандомні числа в діапазоні від 0 до 100 - Math.round(Math.random()*100)) та виводить його.
-
-
+// function numRandom(arr){
+//     for (let i=0; i<100; i++) {
+//         arr[i] = Math.round(Math.random()*100);
+//
+//             }
+//     return arr;
+// }
+// numRandom(20)
 
 // - створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit. limit - аргумент, який характеризує кінцеве значення діапазону.
 
-
+// function numLim(arr,limit) {
+//
+//   for (let i=0; i<limit; i++){
+//         arr[i]= Math.round(Math.random()*limit);
+//
+//   }
+//  return arr;
+// }
+// numLim(30,20)
 
 // - Функція приймає масив та робить з нього новий масив в зворотньому порядку. [1,2,3] -> [3, 2, 1].
+// function printNewMas(arr) {
+//     let newMas =[];
+//     for (let item = arr.length-1; item>=0; item--) {
+//         let newArr = arr[item];
+//         newMas.push(newArr);
+//     }
+//     return newMas;
+// }
+// printNewMas(8,3,5,9,1)
--- lesson4/homework4/main.js
+++ lesson4/homework4/main.js
@@ -1,11 +1,77 @@
 //
 // - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+
+// function areaRect(a,b) {
+//     c=a*b;
+//     return c;
+// }
+// areaRect(4,8);
+// console.log(c)
 // - створити функцію яка обчислює та повертає площу кола з радіусом r
+
+// function areaR(r) {
+//     return Math.PI*r*r;
+// }
+// areaR(3);
+
 // - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
+
+// function sCylinder(h,r) {
+//     return 2*Math.PI*r*h;
+// }
+// sCylinder(5,2)
+
 // - створити функцію яка приймає масив та виводить кожен його елемент
+
+// function print(arr) {
+//     for (const item of arr) {
+//         console.log(item);
+//     }
+// }
+
 // - створити функцію яка створює параграф з текстом. Текст задати через аргумент
+
+// function aboutP(text) {
+//     document.write(`<p>${text}</>`);
+// }
+// aboutP('Hello')
+
+
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
+// function createUlLi(text) {
+//     document.write(`<ul>`);
+//     document.write(`<li>${text}</li>`);
+//     document.write(`<li>${text}</li>`);
+//     document.write(`<li>${text}</li>`);
+//     document.write(`</ul>`);
+// }
+// createUlLi('Hello')
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+
+// function createLi(text, size) {
+//     document.write(`<ul>`);
+// for (let i=0; i<size; i++){
+//     document.write(`<li>${text}</li>`)
+// }
+//     document.write(`</ul>`);
+// }
+// createLi('seven', 3)
+
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+// function printArr(arr) {
+//     document.write(`<ul>`);
+//     for (let item of arr) {
+//         document.write(`<li>${item}</li>`);
+//     }
+//     document.write(`</ul>`);
+// }
+// printArr([true,4,'sat'])
 //
 // - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
+
+function printMas(arr) {
+    for (let item of arr) {
+        document.write(`<div>${item.id} - ${item.name} - ${item.age}</div>`);
+    }
+};
+printMas()
